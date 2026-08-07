829c8fd43c839b47d84e942a1e8024864b225f9e
Bexia Release <release@bexiaerp.com>
2026-08-07 04:14:23 +0000

    V5.83.P2B NIP de 4 digitos y sincronizacion de cajeros PDV

--- app/Filament/Resources/PosCashierResource/Pages/CreatePosCashier.php
+++ app/Filament/Resources/PosCashierResource/Pages/CreatePosCashier.php
@@ -8,4 +8,31 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePosCashier extends CreateRecord
 {
     protected static string $resource = PosCashierResource::class;
+
+    protected ?string $pendingPlainPin = null;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $plainPin = trim((string) ($data['plain_pin'] ?? ''));
+
+        $this->pendingPlainPin = $plainPin !== ''
+            ? $plainPin
+            : null;
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        if ($this->pendingPlainPin === null) {
+            return;
+        }
+
+        PosCashierResource::syncPinToLinkedEmployee(
+            $this->record,
+            $this->pendingPlainPin
+        );
+
+        $this->pendingPlainPin = null;
+    }
 }
